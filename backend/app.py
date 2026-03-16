f6a7635ce5e9bf693c7d655bce4a6108caac555a
annumd <161940207+annumd@users.noreply.github.com>
2026-03-16 15:33:51 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,3 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import os
-import joblib
-import numpy as np
-import uuid
-
-# import feature extraction
-from model.feature_extraction import extract_features
-
-app = Flask(__name__)
-CORS(app)
-
-UPLOAD_FOLDER = "uploads"
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-# Load trained model
-model = joblib.load("model/model.pkl")
-
-@app.route("/")
-def home():
-    return "AI Voice Spoof Detection Backend Running!"
-
 @app.route("/detect", methods=["POST"])
 def detect():
     try:
@@ -30,33 +6,33 @@ def detect():
 
         file = request.files["audio"]
 
-        # generate unique filename
         filename = str(uuid.uuid4()) + "_" + file.filename
         file_path = os.path.join(UPLOAD_FOLDER, filename)
-
         file.save(file_path)
 
         # extract features
         features = extract_features(file_path)
-        if features is None or len(features) == 0:
-            return jsonify({"error": "Audio processing failed"}), 400
+
+        if len(features) != 59:
+            return jsonify({"error": "Feature extraction failed"}), 400
+
         features = np.array(features).reshape(1, -1)
 
-        # predict
+        # prediction
         prediction = model.predict(features)[0]
+        confidence = model.predict_proba(features)[0].max()
+        confidence = round(confidence * 100, 2)
 
         result = "Spoofed Voice" if prediction == 1 else "Genuine Voice"
 
-        # delete file after processing
         if os.path.exists(file_path):
             os.remove(file_path)
 
-
-        return jsonify({"result": result})
+        return jsonify({
+            "result": result,
+            "confidence": f"{confidence}%"
+        })
 
     except Exception as e:
         print("ERROR:", str(e))
         return jsonify({"error": str(e)})
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
