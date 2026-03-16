b7d2105d08cb54e31390a075e024a2ab372d7913
annumd <161940207+annumd@users.noreply.github.com>
2026-03-16 15:21:01 +0530

    Update train_model.py

--- backend/model/train_model.py
+++ backend/model/train_model.py
@@ -2,10 +2,11 @@ import os
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import joblib
 from feature_extraction import extract_features
 
-DATASET_PATH = "dataset"   # your dataset folder
+DATASET_PATH = "dataset"
 
 X = []
 y = []
@@ -15,7 +16,11 @@ for label in ["real", "spoof"]:
 
     for file in os.listdir(folder):
         path = os.path.join(folder, file)
+
         features = extract_features(path)
+
+        if features is None:
+            continue
 
         X.append(features)
         y.append(0 if label == "real" else 1)
@@ -23,14 +28,16 @@ for label in ["real", "spoof"]:
 X = np.array(X)
 y = np.array(y)
 
-# split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# train
 model = RandomForestClassifier(n_estimators=100)
 model.fit(X_train, y_train)
 
-# save
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Model Accuracy:", accuracy)
+
 joblib.dump(model, "model.pkl")
 
 print("Model trained and saved!")
